bb8bc5b0f2602fd0263aed7787f50b3ee053b49a
Alexey Nikolaev <aleksey.nikolaev.nn@gmail.com>
2023-01-04 23:25:32 +0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+if __name__ == '__main__':
+    setup(
+        name='yolov7',
+        version='1.0.0',
+        description='Implementation of paper - YOLOv7: Trainable bag-of-freebies sets new state-of-the-art for real-time object detectors',
+        author='Kin-Yiu, Wong',
+        url='https://github.com/WongKinYiu/yolov7',
+        packages=find_packages()
+    )
